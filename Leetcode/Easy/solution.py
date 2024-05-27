2da67fcfa8380f4ecb23c32e5c1852cb54f32dc0
Waheeb Barqawi <trolltrolltrolllollollol@outlook.com>
2024-05-26 20:00:19 -0400

    imporved Big Countries solution

--- Leetcode/Easy/solution.py
+++ Leetcode/Easy/solution.py
@@ -1,5 +1,4 @@
 import pandas as pd
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    world_with_big_countries = world[((world['area'] >= 3000000) | (world['population'] >= 25000000))]
-    return world_with_big_countries[['name', 'area', 'population']]
+    return world[((world['area'] >= 3000000) | (world['population'] >= 25000000))][['name', 'area', 'population']]
